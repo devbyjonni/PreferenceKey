0b16cf1ce686099bc7f595633d0f5a1ed7f7a6df
Jonni Åkesson <enterthesource@me.com>
2022-10-12 18:43:27 +0200

    Initial Commit

---
+++ PreferenceKey/PreferenceKeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PreferenceKeyApp.swift
+//  PreferenceKey
+//
+//  Created by Jonni Akesson on 2022-10-12.
+//
+
+import SwiftUI
+
+@main
+struct PreferenceKeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
